2d3cb8959cc3aea36fbd7076bd9ca0122e957cff
Anthony Tilelli <anthony@tilelli.me>
2020-02-13 02:47:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,6 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-    
     # binding.pry
     if user && user.authenticate(params[:password])
       session[:id] = user.id
